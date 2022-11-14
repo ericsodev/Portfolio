0a1cf954cf40443bca1645a2fd627b215cdd6ac4
Eric So <eric.so.dev@gmail.com>
2022-11-14 16:35:26 -0500

    card hover effect

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ import "../styles/projects.css";
 import { SectionLayout } from "./layout/SectionLayout";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LanguageIcon from "@mui/icons-material/Language";
+import React from "react";
 
 const exampleProjects = [
   {
@@ -24,9 +25,13 @@ export function Projects({ className }: { className?: string }): JSX.Element {
   return (
     <SectionLayout title={"creations"}>
       <div className={`projects ${className}`}>
-        <div className="cards">
-          {exampleProjects.map((proj) => {
-            return <Card {...proj}></Card>;
+        <div
+          className="cards"
+          onMouseMove={handleCardHover}
+          onMouseLeave={handleCardExit}
+        >
+          {exampleProjects.map((proj, i) => {
+            return <Card key={i} index={i} {...proj}></Card>;
           })}
         </div>
       </div>
@@ -34,15 +39,34 @@ export function Projects({ className }: { className?: string }): JSX.Element {
   );
 }
 
+function handleCardHover(e: React.MouseEvent) {
+  for (let i = 0; i < exampleProjects.length; i++) {
+    const card = document.getElementById(`card-${i}`);
+    const rect = card?.getBoundingClientRect();
+    if (!rect) continue;
+    card?.style.setProperty("--mouse-x", `${e.clientX - rect.x}px`);
+    card?.style.setProperty("--mouse-y", `${e.clientY - rect.y}px`);
+  }
+}
+
+function handleCardExit(e: React.MouseEvent) {
+  for (let i = 0; i < exampleProjects.length; i++) {
+    const card = document.getElementById(`card-${i}`);
+    card?.style.setProperty("--mouse-x", ``);
+    card?.style.setProperty("--mouse-y", ``);
+  }
+}
+
 interface ICard {
   title: string;
   desc?: string;
   tools: string[];
   children?: React.ReactNode;
+  index: number;
 }
-function Card({ title, desc, tools, children }: ICard): JSX.Element {
+function Card({ index, title, desc, tools, children }: ICard): JSX.Element {
   return (
-    <div className="card">
+    <div id={`card-${index}`} className="card">
       <div className="card-header">
         <h3 className="card-title">{title}</h3>
         <div className="card-icons">
